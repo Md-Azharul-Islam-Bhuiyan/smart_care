0b4ae4ad8079b8b3f1e712da8128f67c39c94a30
Md Azharul Islam Bhuiyan <mdazharulislambhuiyaniiuc@gmail.com>
2024-01-14 12:37:26 +0600

    Updated

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ic7n7!l7hcjkdvz@mfr&c@!i%3*xo51ep=@n&43^)u9^oqzqzb
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = ['https://smart-care-d2nr.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
